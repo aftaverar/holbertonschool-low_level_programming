b067e28e22987375b6b761d02ef86a05fc99f65d
aftaverar <andres.felipe.reyes.90@gmail.com>
2020-06-10 21:58:44 -0500

    Fixed the 4 file, passed betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - First function in the program that is executed - prints "Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual puzzle
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
