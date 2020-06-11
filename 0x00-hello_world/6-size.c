a42ad99013bab02668c5e06d902b3403db54dc01
aftaverar <andres.felipe.reyes.90@gmail.com>
2020-06-10 23:20:18 -0500

    Added exercise 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - PRINTS the size of varius types
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %ld byte\n", sizeof(char Type));
+printf("Size of a int: %ld bytes\n", sizeof(int Type));
+printf("Size of a long int: %ld bytes\n", sizeof(long int Type));
+printf("Size of a long long int: %ld bytes\n", sizeof(long long int Type));
+printf("Size of a float: %ld bytes\n", sizeof(float Type));
+return (0);
+}
